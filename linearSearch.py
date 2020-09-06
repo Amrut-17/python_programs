c68e1395365fbff4e812d69d71e02e3150a7543e
Amrut-17 <68114007+Amrut-17@users.noreply.github.com>
2020-09-06 13:34:52 +0530

    Basic DATA_STRUCTURE concept. Linear Search

---
+++ linearSearch.py
@@ -0,0 +1,23 @@
+def linear(array):
+    print('ENTER ARRAY ELEMENT ONE BY ONE\t ')
+    for i in range (n):
+        array[i] = int (input())
+
+    print ('ARRAY ELEMENTS ARE :\n\t= ',array)
+    elem = [int (input('ENTER ELEMENT TO SEARCH\n\t'))]
+    found = False
+    while i<=n:
+        for i in range (n):
+            if array[i]==elem:
+                found=True
+                print ('ELEMENT IS FOUND AT LOCATION ',i)
+        break
+    if not found :
+        print('ELEMENT IS NOT FOUND ')
+
+import numpy
+n = int (input ('ENTER ARRAY SIZE \n'))
+array = numpy.ndarray(shape = (n) , dtype = int)
+linear(array)
+            
+		
