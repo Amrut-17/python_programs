a3511b6af5d3b79e802cc9b35bf1478d3fa81292
Amrut-17 <68114007+Amrut-17@users.noreply.github.com>
2020-12-18 11:31:32 +0530

    first commit
    Data-Structure :
    -- Merge Sort

---
+++ Merge_Sort.py
@@ -0,0 +1,36 @@
+def merge_sort(list):
+    if len(list) > 1:
+        mid = len(list)//2
+        left = list[:mid]
+        right = list[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = 0
+        j = 0 
+        k = 0
+    
+        while i < len(left) and j < len(right) :
+            if left[i] < right[j] :
+                list[k] = left[i]
+                i+= 1
+                k+= 1
+            else :
+                list[k] = right[j]
+                j+= 1
+                k+= 1
+            
+        while i < len(left) :
+            list[k] = left[i]
+            i+= 1
+            k+= 1
+    
+        while j < len(right) :
+            list[k] = right[j]
+            j+= 1
+            k+= 1
+        
+n = int (input('enter size \n'))
+print ('enter values one by one \n')
+list = [int (input ()) for x in range (n)]
+merge_sort(list)
+print('sorted list : ' , list)        
